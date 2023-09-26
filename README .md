82b6f6dc00d6c08e2febdab36cdf11e799c92b0d
Yakitiyak <115512498+Jason-Chamberlain@users.noreply.github.com>
2023-09-25 17:27:19 -0700

    Initial Upload

---
+++ README .md
@@ -0,0 +1,29 @@
+# C# .NET Core Scores Application
+This repository contains a simple C# .NET Core application named "Scores" that was created as part of a C# .NET course from The [Tech Academy](https://www.learncodinganywhere.com/). In this README, an overview will be provided regarding what was learned while working on this project.
+
+## About the Application
+The "Scores" application is a console-based program that interacts with text files containing student scores. The following tasks are performed:
+
+1. The user is greeted, and the current date is displayed.
+1. Student scores are read from a text file.
+1. The total number of scores and the average score are calculated and displayed.
+1. User input is awaited before exiting.
+
+## What Was Learned
+### 1. **Console Input and Output**
+- The `Console.WriteLine` and `Console.ReadLine` methods were employed to display messages to the user and collect input from them.
+### 2. String Interpolation
+- String interpolation was used to create dynamic messages, with user input and the current date being incorporated into the output.
+### 3. File I/O
+- The ability to read data from text files was demonstrated, employing the `File.ReadAllLines` method.
+### 4. Data Conversion
+- String data from the file was transformed into numerical values (double) through the use of `Convert.ToDouble` in order to execute calculations.
+### 5. Data Processing
+- The total score and average score of the students were computed.
+### 6. Basic Control Flow
+- Loops and conditional statements were utilized to govern the execution flow of the program.
+### 7. Error Handling (Potential Improvement)
+- Although not explicitly incorporated in this example, error handling mechanisms, such as try-catch blocks, could be added to manage potential exceptions when converting strings to doubles, with the aim of making the program more robust.
+
+## Conclusion
+The "Scores" application serves as a simple demonstration of core C# and .NET Core concepts, including input/output, file handling, data conversion, and basic control flow. It showcases the capacity to create interactive console applications in C#, utilizing foundational skills in C# and .NET Core, and offers the potential for further improvements and enhancements to handle real-world scenarios and larger datasets.
---
+++ Scores/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Please enter your first name");          // Prompt user to enter their first name            
+            string date = DateTime.Today.ToShortDateString();           // Get current date in short date format (MM/dd/yyyy)            
+            string uName = Console.ReadLine();                          // Read user's input for first name            
+            string msg = $"\nWelcome back {uName}. Today is {date}.";   // Create welcome message using user's name and current date            
+            Console.WriteLine(msg);                                     // Display welcome message
+
+            // Define the path to the text file containing student scores
+            string path = @"C:\Users\jason\OneDrive\Desktop\C#_ASP.NET_Core_Scores\Scores\Scores\studentScores.txt";            
+            string[] lines = File.ReadAllLines(path);                   // Read all lines from specified text file into array of strings
+
+            double tScore = 0.0;
+            
+            Console.WriteLine("\nStudent Scores:  \n");                 // Display header for the list of student scores            
+            foreach (string line in lines)                              // Loop through each line in file
+            {                
+                Console.Write("\n" + line);                             // Display current score                
+                double score = Convert.ToDouble(line);                  // Convert line (score) to double                
+                tScore += score;                                        // Add score to tScore (total score)
+            }
+            
+            double avgScore = tScore / lines.Length;                    // Calculate average score
+           
+            Console.WriteLine("\nTotal of " + lines.Length +            // Display total number of student scores and average score
+                              " student scores. " + "\t Average score: " + avgScore);            
+            Console.WriteLine("\n\nPress any key to exit.");            // Prompt user to press any key to exit            
+            Console.ReadKey();                                          // Wait for a key press before exiting
+        }
+    }
+}
